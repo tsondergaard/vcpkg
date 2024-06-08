03d128929dae58413bc5ab3bd5002a6c004c3074
Thomas Sondergaard <thomas.sondergaard@mi.medical.canon>
2024-06-08 17:04:41 +0200

    Copy of the qtwebengine overlay from my project

--- ports/qtwebengine/portfile.cmake
+++ ports/qtwebengine/portfile.cmake
@@ -1,13 +1,14 @@
 set(SCRIPT_PATH "${CURRENT_INSTALLED_DIR}/share/qtbase")
 include("${SCRIPT_PATH}/qt_install_submodule.cmake")
 
-set(${PORT}_PATCHES 
+set(${PORT}_PATCHES
       "clang-cl.patch"
       "adjust-pkg-config.patch"
       "vcpkg-dirs.patch"
+      "qtbug-120248-disable-cups.patch"
 )
 
-set(TOOL_NAMES gn QtWebEngineProcess qwebengine_convert_dict webenginedriver)
+set(TOOL_NAMES gn qwebengine_convert_dict webenginedriver)
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
 FEATURES
@@ -43,7 +44,7 @@ if(VCPKG_TARGET_IS_LINUX)
     # qt_configure_add_summary_entry(ARGS "webengine-system-libpci")
     # + ALSA and PULSEAUDIO
 
-    set(system_libs re2 icu libwebp opus ffmpeg libvpx snappy glib zlib minizip libxml libpng libjpeg harfbuzz freetype lcms2 libtiff libopenjpeg2) #libevent
+    set(system_libs re2 icu libwebp opus libvpx snappy glib zlib minizip libxml libpng libjpeg harfbuzz freetype lcms2 libtiff libopenjpeg2) #libevent
     foreach(_sys_lib IN LISTS system_libs)
         list(APPEND FEATURE_OPTIONS "-DFEATURE_webengine_system_${_sys_lib}=ON")
     endforeach()
@@ -59,10 +60,6 @@ find_program(NODEJS NAMES node PATHS "${CURRENT_HOST_INSTALLED_DIR}/tools/node" 
 #find_program(NODEJS NAMES node)
 if(NOT NODEJS)
     message(FATAL_ERROR "node not found! Please install it via your system package manager!")
-endif()
-
-if(NOT VCPKG_TARGET_IS_WINDOWS)
-  vcpkg_add_to_path(PREPEND "${CURRENT_HOST_INSTALLED_DIR}/tools/cups/bin")
 endif()
 
 get_filename_component(NODEJS_DIR "${NODEJS}" DIRECTORY )
@@ -111,9 +108,6 @@ endif()
 if(NOT VCPKG_TARGET_IS_WINDOWS)
   find_program(pkgconf NAMES pkgconf pkg-config REQUIRED)
   set(ENV{PKG_CONFIG} "${pkgconf}")
-  set(ENV{LD_LIBRARY_PATH} "${CURRENT_INSTALLED_DIR}/lib")
-  set(ENV{CFLAGS} "-Wl,-rpath,${CURRENT_INSTALLED_DIR}/lib")
-  set(ENV{CXXFLAGS} "-Wl,-rpath,${CURRENT_INSTALLED_DIR}/lib")
 endif()
 
 qt_cmake_configure( DISABLE_PARALLEL_CONFIGURE # due to in source changes.
@@ -127,19 +121,11 @@ qt_cmake_configure( DISABLE_PARALLEL_CONFIGURE # due to in source changes.
                         -DQT_FEATURE_webengine_jumbo_build=0
                         -DCMAKE_INSTALL_RPATH=${CURRENT_INSTALLED_DIR}/lib
                         -DCMAKE_BUILD_RPATH=${CURRENT_INSTALLED_DIR}/lib
-                        #"-DCMAKE_C_FLAGS_RELEASE=-I${CURRENT_INSTALLED_DIR}/include -Wl,-rpath,${CURRENT_INSTALLED_DIR}/lib"
-                        #"-DCMAKE_CXX_FLAGS_RELEASE=-I${CURRENT_INSTALLED_DIR}/include -Wl,-rpath,${CURRENT_INSTALLED_DIR}/lib"
                    OPTIONS_DEBUG ${_qis_CONFIGURE_OPTIONS_DEBUG}
                    OPTIONS_RELEASE ${_qis_CONFIGURE_OPTIONS_RELEASE})
 
 vcpkg_cmake_install(ADD_BIN_TO_PATH)
 
 qt_fixup_and_cleanup(TOOL_NAMES ${TOOL_NAMES})
-
-if(VCPKG_TARGET_IS_WINDOWS AND NOT VCPKG_BUILD_TYPE)
-    file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/tools/Qt6/bin/debug/")
-    file(RENAME "${CURRENT_PACKAGES_DIR}/debug/bin/QtWebEngineProcessd.exe" "${CURRENT_PACKAGES_DIR}/tools/Qt6/bin/debug/QtWebEngineProcessd.exe")
-    file(RENAME "${CURRENT_PACKAGES_DIR}/debug/bin/QtWebEngineProcessd.pdb" "${CURRENT_PACKAGES_DIR}/tools/Qt6/bin/debug/QtWebEngineProcessd.pdb")
-endif()
 
 qt_install_copyright("${SOURCE_PATH}")
